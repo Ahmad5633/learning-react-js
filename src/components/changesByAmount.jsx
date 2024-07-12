569fee1ca3efdbfb6e1c2ea12e8e3b93ae97f415
Ahmad5633 <ahmadhameed5633@gmail.com>
2024-07-12 10:41:45 +0500

    Redux Implemented

---
+++ src/components/changesByAmount.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { incrementByAmount } from "../redux/app";
+
+export default function ChangesByAmount() {
+  const dispatch = useDispatch();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const amount = parseInt(event.target.number.value);
+    if (!isNaN(amount)) {
+      dispatch(incrementByAmount(amount));
+    }
+    event.target.number.value = "";
+  };
+
+  return (
+    <form onSubmit={handleClick}>
+      <input type="number" name="number" placeholder="Change by Amount" />
+      <button type="submit">Change by amount</button>
+    </form>
+  );
+}
---
+++ src/pages/counter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CounterText from "../components/counterText";
+import DecrementButton from "../components/decrementButton";
+import IncrementButton from "../components/incrementButton";
+import ChangesByAmount from "../components/changesByAmount";
+
+export default function Counter() {
+  return (
+    <div>
+      <DecrementButton />
+      <CounterText />
+      <IncrementButton />
+      <ChangesByAmount />
+    </div>
+  );
+}
